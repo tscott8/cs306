06fa27271f44e89ba51281039911d43b36252298
Tyler Scott <tybugman@hotmail.com>
2016-12-07 16:45:15 -0700

    better pair

--- 3rd_exp/shuffles.py
+++ 3rd_exp/shuffles.py
@@ -17,7 +17,12 @@ def riffle_shuffle(code):
         deck.insert(insert_index, item)
     return (''.join(deck))
 
+    
+def rotate(strg,n):
+    return strg[n:] + strg[:n]
+    
 def paired_shuffle(code):
+    code=rotate(code,1)
     n = 2
     if type(code) is not str:
         code = ''.join(code)
@@ -25,13 +30,21 @@ def paired_shuffle(code):
     rand.shuffle(code)
     return (''.join(code))
     
-    
 def rif_n(code, n):
     while n > 0:
         code = riffle_shuffle(code)
         n -= 1
     return code
+
+def pair_n(code, n):
+    while n > 0:
+        code = paired_shuffle(code)
+        n -= 1
+    return code
 shuf = rif_n('abcdefghijklmnopqrst', rand.randint(1,10))
+print(shuf)
+
+shuf = pair_n('abcdefghijklmnopqrst', rand.randint(1,10))
 print(shuf)
 #print(riffle_shuffle('abcdefg'))
 #print(paired_shuffle('abcdefg'))
